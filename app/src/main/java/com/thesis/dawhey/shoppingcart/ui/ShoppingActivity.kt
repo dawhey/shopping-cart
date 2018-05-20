b12b5230cdd4d5b42a95a6e5949c0d5df9fe9c6f
Dawid Heyman <dawhey@gmail.com>
2018-05-20 17:53:11 +0200

    Added recycler view

--- app/src/main/java/com/thesis/dawhey/shoppingcart/ui/ShoppingActivity.kt
+++ app/src/main/java/com/thesis/dawhey/shoppingcart/ui/ShoppingActivity.kt
@@ -1,30 +1,46 @@
 package com.thesis.dawhey.shoppingcart.ui
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import com.thesis.dawhey.shoppingcart.R
+import com.thesis.dawhey.shoppingcart.adapters.ProductsAdapter
 import com.thesis.dawhey.shoppingcart.prefs
 import com.thesis.dawhey.shoppingcart.repositories.DataRepository
 import com.thesis.dawhey.shoppingcart.repositories.DataRepositoryImpl
 import com.thesis.dawhey.shoppingcart.request.GetScannedProductsRequest
 import com.thesis.dawhey.shoppingcart.response.GetScannedProductsResponse
 import com.thesis.dawhey.shoppingcart.viewmodels.ShoppingViewModel
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : RequestResponseActivity<GetScannedProductsResponse, GetScannedProductsRequest, ShoppingViewModel>() {
 
-    val dataRepository: DataRepository = DataRepositoryImpl()
+    private val dataRepository: DataRepository = DataRepositoryImpl()
+
+    private val adapter: ProductsAdapter by lazy {
+        ProductsAdapter()
+    }
 
     override fun provideViewModel(): ShoppingViewModel = ViewModelProvider.AndroidViewModelFactory(application).create(ShoppingViewModel::class.java)
 
-    override fun provideToolbarTitle(): String = "Scanned products"
+    override fun provideToolbarTitle(): String = getString(R.string.scanned_products)
 
     override fun provideLayoutResource(): Int = R.layout.activity_shopping
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.subtitle = "Shopping cart ID: " + prefs.cartId
+        supportActionBar?.subtitle = getString(R.string.cart_id_subtitle) + prefs.cartId
+        productsView.layoutManager = LinearLayoutManager(this, 0, false)
+
+        viewModel.products.observe(this, Observer {
+            with(adapter) {
+                products = it!!
+                notifyDataSetChanged()
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -41,5 +57,14 @@ class ShoppingActivity : RequestResponseActivity<GetScannedProductsResponse, Get
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.request()
+    }
+
+    override fun onLoading() {
+        swipeRefreshView.isRefreshing = true
     }
 }
--- app/src/main/java/com/thesis/dawhey/shoppingcart/viewmodels/ShoppingViewModel.kt
+++ app/src/main/java/com/thesis/dawhey/shoppingcart/viewmodels/ShoppingViewModel.kt
@@ -1,6 +1,9 @@
 package com.thesis.dawhey.shoppingcart.viewmodels
 
 import android.app.Application
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import com.thesis.dawhey.shoppingcart.models.Product
 import com.thesis.dawhey.shoppingcart.prefs
 import com.thesis.dawhey.shoppingcart.repositories.DataRepository
 import com.thesis.dawhey.shoppingcart.repositories.DataRepositoryImpl
@@ -10,10 +13,17 @@ import io.reactivex.Single
 
 class ShoppingViewModel(application: Application) : RequestResponseViewModel<GetScannedProductsResponse, GetScannedProductsRequest>(application) {
 
+    val products = MutableLiveData<List<Product>>()
+
     val dataRepository: DataRepository = DataRepositoryImpl()
 
     override var request: GetScannedProductsRequest = GetScannedProductsRequest(prefs.token)
 
     override fun provideObservableResultData(request: GetScannedProductsRequest): Single<GetScannedProductsResponse> =
             dataRepository.getScannedProducts(request)
+
+    override fun onSuccess(response: GetScannedProductsResponse) {
+        super.onSuccess(response)
+        products.value = response.products
+    }
 }
